d6dac607599729bfa8d4613771e621ea5f1054da
dev.xxolkim <dev.xxolkim@gmail.com>
2023-11-13 11:19:02 +0900

    feat : CloudType 배포 테스트

---
+++ swagger/interpark.swagger.js
@@ -0,0 +1,33 @@
+/**
+ * @swagger
+ * /visual:
+ *   get:
+ *     summary: 상품목록
+ *     tags: [Interpark]
+ *     responses:
+ *       200:
+ *         description: 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                  good_count:
+ *                      type: int
+ *                      example: 1
+ *                  good_1:
+ *                      example:
+ *                        link: "#"
+ *                        pic: good1.png
+ *                        product: 맥 MAC 립스틱
+ *                        ratio: 5
+ *                        price: 11,950
+ *                  good_2:
+ *                      example:
+ *                        link: "#"
+ *                        pic: good1.png
+ *                        product: 맥 MAC 립스틱
+ *                        ratio: 5
+ *                        price: 11,950
+ *
+ */
